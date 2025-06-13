f5c09d19a5e43bb0b03c2f98a755e92ce39d1d79
pchapet <gabriel63370@gmail.com>
2025-06-13 12:11:07 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>La Terrasse du Parc</h1>
+      <p>Bienvenue dans notre appartement de location à Lempdes (63) !</p>
+    </div>
+  );
+}
